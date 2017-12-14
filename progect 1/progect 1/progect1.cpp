7d7ce53e7b17b1918a4f5ebba3047ce1ef8228f2
Mike <wregtron@gmail.com>
2017-12-14 20:02:42 +0200

    int b

---
+++ progect 1/progect 1/progect1.cpp
@@ -0,0 +1,15 @@
+// progect1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int a;
+	int b;
+    return 0;
+}
+
